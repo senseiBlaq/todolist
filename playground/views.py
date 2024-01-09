e5a9e48672d16e7bca722c85b9ea7b76176abf8f
sensei_blaq <jnr.amamoo@gmail.com>
2024-01-09 02:14:22 +0000

    trying httoResponse in views.py, creating dynamic path using inputs

--- playground/views.py
+++ playground/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
 def index (request):
-    return HttpResponse('Hello, world')
+    return HttpResponse ('Hello, world!')
+
+def greeting (request, name):
+    return HttpResponse (f'Hello, {name}!')
--- todolist/urls.py
+++ todolist/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('playground', include('playground.urls')),
+    path('playground/', include('playground.urls')),
     path('admin/', admin.site.urls),
 ]
